ef2a8f3617d391c3457ab8eb1cbce35a647b7f85
mjackson45 <mjackson@gatech.edu>
2016-09-06 01:19:08 -0400

    Implemented waitlist

--- helloworld/src/helloworld.c
+++ helloworld/src/helloworld.c
@@ -7,27 +7,21 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
-
-//test
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-int CAPACITY = 5;
+int CAPACITY = 2;
 int TIME_TO_EAT = 10;
 int num_diners = 0;
 int diners_arrived = 0;
-
 typedef struct Diner {
 	int id;
 	double time_waited;
 } Diner;
 
-Diner *diners[3];
+Diner *diners[10];
 
 
 int get_total_diners(){
@@ -49,6 +43,8 @@ typedef struct {
 	int len;
 	int size;
 } heap_t;
+
+heap_t *waitlist_list;
 
 void push(heap_t *h, int priority, Event *data) {
 	if (h->len + 1 >= h->size) {
@@ -96,8 +92,9 @@ Event *pop(heap_t *h) {
 	return data;
 }
 
-void waitlist(){
-
+void waitlist(Event *e){
+	e->diner->time_waited = e->time;
+	push(waitlist_list, e->time, e);
 }
 void arrival(heap_t *events, Event *e) {
 
@@ -105,6 +102,7 @@ void arrival(heap_t *events, Event *e) {
 	diners_arrived ++;
 
 	if (num_diners < CAPACITY) {
+		num_diners++;
 		double departure_time = e->time + TIME_TO_EAT;
 		Event *departure = (Event *) malloc(sizeof(Event));
 		departure->time = departure_time;
@@ -113,7 +111,7 @@ void arrival(heap_t *events, Event *e) {
 		push(events, departure->time, departure);
 		printf("Diner %d was seated at t=%f. Wait time = 0\n", e->diner->id, e->time);
 	} else {
-		waitlist();
+		waitlist(e);
 	}
 	if(diners_arrived < get_total_diners()){
 		Event *next_arrival = malloc(sizeof(Event));
@@ -127,20 +125,34 @@ void arrival(heap_t *events, Event *e) {
 void departure(heap_t *events, Event *e){
 	num_diners--;
 	printf("Diner %d departed at t=%f!\n", e->diner->id,e->time);
-
+	if(waitlist_list->len){
+		Event *offlist = pop(waitlist_list);
+		offlist->diner->time_waited = e->time - offlist->diner->time_waited;
+		double departure_time = e->time + TIME_TO_EAT;
+		Event *new_departure = (Event *) malloc(sizeof(Event));
+		new_departure->time = departure_time;
+		new_departure->type = "departure";
+		new_departure->diner = offlist->diner;
+		push(events, new_departure->time, new_departure);
+		printf("Diner %d was seated at t=%f. Wait time = %f\n", offlist->diner->id, e->time, offlist->diner->time_waited);
+		num_diners++;
+	}
 }
 
 int main(void) {
-// setup
+	// setup
 	srand(time(NULL));
 	double now = 0;
-	for (size_t i = 0; i < sizeof diners / sizeof *diners; i++){
+	int id_num = 1;
+	int total_diners = sizeof diners / sizeof(*diners);
+	for (size_t i = 0; i < total_diners; i++){
 		Diner *d = (Diner *) malloc(sizeof(Diner));
-		d->id=++num_diners;
+		d->id=id_num++;
 		d->time_waited=0;
 		diners[i] =d;
 	}
 	heap_t *events = (heap_t *) calloc(1, sizeof(heap_t));
+	waitlist_list = (heap_t *) calloc(1, sizeof(heap_t));
 	Event *first_arrival = (Event *) malloc(sizeof(Event));
 	first_arrival->time = now;
 	first_arrival->type = "arrival";
@@ -164,6 +176,10 @@ int main(void) {
 	 increment waiting list times
 
 	 */
-
+	double total_wait = 0;
+	for(int i=0; i < total_diners; i++){
+		total_wait +=diners[i]->time_waited;
+	}
+	printf("Average wait for %d diners = %f",total_diners, total_wait/total_diners);
 	return EXIT_SUCCESS;
 }
